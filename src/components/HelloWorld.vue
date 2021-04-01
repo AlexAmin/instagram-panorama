11c082e39b08c655c5f5b15862d88f3701ad356c
Alexander Amin <alex@alexamin.de>
2021-04-01 10:38:43 +0200

    Added empty / loading states

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,56 +4,64 @@
       <b-jumbotron class="upload-jumbotron">
         <b-row>
           <b-col sm="12"><h1>Use your panoramas on Insta</h1></b-col>
+          <b-col sm="12"><p>
+            Ever wanted to use panoramas on Instagram but got frustrated trying to trim them down to fit?
+            <br/>
+            Select the panorama you would like to convert and we'll cut it down into a few square-sized photos that will work on Instagram
+            <br/>
+            And the best part? Your data never leaves your computer. Your privacy matters!
+          </p></b-col>
           <b-col sm="12">
             <b-form-group>
               <b-form-file id="file-large" size="lg"  accept="image/*" @change="fileSelected"/>
             </b-form-group>
-            <button @click="download">Download</button>
           </b-col>
         </b-row>
       </b-jumbotron>
     </b-col>
-    <b-col sm="6">
-      <b-col sm="12">Original</b-col>
-      <b-col sm="12"
-             class="carousel-container"
-             :style="`background-image:url(${inputImage};`"/>
+    <b-col sm="12" v-if="!inputFile">none</b-col>
+    <b-col sm="12" v-else-if="processing">
+      <p>Loading</p>
     </b-col>
-    <b-col sm="6">
-      <b-row>
-        <b-col sm="12">Panorama</b-col>
+    <slot v-else>
+      <b-col sm="6">
+        <b-col sm="12">Original</b-col>
         <b-col sm="12"
-               v-if="previews.length > 0"
                class="carousel-container"
-               :style="`background-image:url(${loadImage(previews[previewIndex])};`">
-          <div class="carousel-buttons">
+               :style="`background-image:url(${inputImage};`"/>
+      </b-col>
+      <b-col sm="6">
+        <b-row>
+          <b-col sm="12">Panorama</b-col>
+          <b-col sm="12"
+                 v-if="previews.length > 0"
+                 class="carousel-container"
+                 :style="`background-image:url(${loadImage(previews[previewIndex])};`">
+            <div class="carousel-buttons">
             <span
                 v-if="nextImageAvailable"
                 @click="previousImage" class="carousel-button carousel-button-left">&lt;</span>
-            <span
-                v-if="nextImageAvailable"
-                @click="nextImage" class="carousel-button carousel-button-right">&gt;</span>
-          </div>
-        </b-col>
-        <b-col sm="12">
-          <ul>
-          <li
-              v-for="i in previews.length"
-              :key="i+'-preview-dot'"
-              style="display: inline-block; height: 8px; width: 8px; margin-left: 4px; border-radius: 16px; background-color: grey;"
-              v-bind:class="{dotActive: i === previewIndex+1}"
-          />
-          </ul>
-        </b-col>
-      </b-row>
-    </b-col>
-    <b-col cols="12" sm="6" v-if="processing">
-      <p>loading</p>
-    </b-col>
-    <b-col v-else sm="6" cols="12">
-      <p>{{previewIndex}}</p>
-
-    </b-col>
+              <span
+                  v-if="nextImageAvailable"
+                  @click="nextImage" class="carousel-button carousel-button-right">&gt;</span>
+            </div>
+          </b-col>
+          <b-col sm="12">
+            <ul>
+              <li
+                  v-for="i in previews.length"
+                  :key="i+'-preview-dot'"
+                  style="display: inline-block; height: 8px; width: 8px; margin-left: 4px; border-radius: 16px; background-color: grey;"
+                  v-bind:class="{dotActive: i === previewIndex+1}"
+              />
+            </ul>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col sm="6" cols="12">
+        <p>{{previewIndex}}</p>
+      </b-col>
+    </slot>
     <!-- Used for image editing -->
     <img style="display: none;" ref="imageFile" alt="hidden preview" src="#"/>
   </b-row>
@@ -99,8 +107,10 @@ export default defineComponent({
           element.style.display = 'none';
           document.body.appendChild(element);
           element.click();
-          document.body.removeChild(element);
-        }, 1500 * index)
+          setTimeout(()=>{
+            document.body.removeChild(element);
+          }, 10000)
+        }, 200 * index)
 
       })
     }
